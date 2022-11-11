788f5a855477afea936384ce55b43aace406d335
Xeostop1 <stop1@daum.net>
2022-11-11 12:03:50 +0900

    add: 컴퍼넌트로 작성자, 일기내용, 점수 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import DirayEditor from './DiaryEditor';
 
 function App() {
   return (
     <div className="App">
-      <h1>드디어 시작한다!!!</h1>
-      <h1>깃이랑 1시간째 싸움하고 있다 참말로</h1>
+      <DirayEditor/>
     </div>
   );
 }
