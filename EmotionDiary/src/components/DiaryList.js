ee189ccc435f77db82b55a204493446f2e3bd88b
hanaseo <11reqeuiem27@gmail.com>
2022-12-09 01:11:11 +0900

    add: css수정, 기능별로 정리

--- EmotionDiary/src/components/DiaryList.js
+++ EmotionDiary/src/components/DiaryList.js
@@ -1,52 +1,97 @@
 // value=option value, onChange=바꾸는 동작, optionList=option
-import { useState } from 'react';
-const sortOptionList=[
-  {value:"latest",name:"최근"},
-  {value:"oldest", name:"등록일"}
+import { useState } from "react";
+import MyButton from "./MyButton";
+import { useNavigate } from "react-router-dom";
+import DiaryItem from "./DiaryItem";
+
+const sortOptionList = [
+  { value: "latest", name: "최근" },
+  { value: "oldest", name: "등록일" },
+];
+
+const filterOptionList = [
+  { value: "all", name: "모두" },
+  { value: "good", name: "긍정" },
+  { value: "bad", name: "부정" },
 ];
 //optionList는 위의 sortOptionList 객체 개별
-const ControlMenu=({value, onChange, optionList})=>{
+
+const ControlMenu = ({ value, onChange, optionList }) => {
   return (
-  <select 
-  value={value} 
-  onChange={(e)=>onChange(e.target.value)}>
-    {optionList.map((it,i)=>(
-      <option key={i} value={it.value}>
-        {it.name}
-        </option>))}
-  </select>
+    <select
+      className="ControlMenu"
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+    >
+      {optionList.map((it, i) => (
+        <option key={i} value={it.value}>
+          {it.name}
+        </option>
+      ))}
+    </select>
   );
-}
+};
 
 const DiaryList = ({ diaryList }) => {
-  const [sortType, setSortType]=useState("latest");
-  const getProcessedDirayList =()=>{
-    
-    //비교 함수
-    const compare=(a,b)=>{
-      if(sortType==='latest'){
-        return parseInt(b.date)-parseInt(a.date);
-      }else{
-        return parseInt(a.date)-parseInt(b.date);
+  const negative = useNavigate();
+  const [sortType, setSortType] = useState("latest");
+  const [filter, setFilter] = useState("all");
+
+  const getProcessedDirayList = () => {
+    //필터링 함수
+
+    const filterCallBack = (item) => {
+      if (filter === "good") {
+        return parseInt(item.emotion) <= 3;
+      } else {
+        return parseInt(item.emotion) > 3;
       }
     };
 
-    const copyList=JSON.parse(JSON.stringify(diaryList));
-    const sortedList=copyList.sort(compare);
-    return sortedList;
-    
-  }
+    //비교 함수
+    const compare = (a, b) => {
+      if (sortType === "latest") {
+        return parseInt(b.date) - parseInt(a.date);
+      } else {
+        return parseInt(a.date) - parseInt(b.date);
+      }
+    };
 
+    const copyList = JSON.parse(JSON.stringify(diaryList));
+
+    //copyList 필터링 작업
+    const filteredList =
+      filter === "all" ? copyList : copyList.filter((it) => filterCallBack(it));
+    const sortedList = filteredList.sort(compare);
+    return sortedList;
+  };
 
   return (
-    <div>
-    <ControlMenu 
-    value={sortType} 
-    onChange={setSortType} 
-    optionList={sortOptionList}/>
+    <div className="DiaryList">
+      <div className="menu_wrapper">
+        <div className="left_col">
+          <ControlMenu
+            value={sortType}
+            onChange={setSortType}
+            optionList={sortOptionList}
+          />
+          <ControlMenu
+            value={filter}
+            onChange={setFilter}
+            optionList={filterOptionList}
+          />
+        </div>
+        <div className="right_col">
+          <MyButton
+            type={"positive"}
+            text={"New Diary"}
+            onClick={() => negative("/new")}
+          />
+        </div>
+      </div>
 
       {getProcessedDirayList().map((it) => (
-        <div key={it.id}>{it.content}</div>
+        <DiaryItem key={it.id} {...it} />
       ))}
     </div>
   );
