e69a807a1898fe58783d5499b1af1eb65d5b4252
Xeostop1 <stop1@daum.net>
2022-12-08 20:05:36 +0900

    add: select tag

--- EmotionDiary/src/components/DiaryList.js
+++ EmotionDiary/src/components/DiaryList.js
@@ -1,7 +1,51 @@
+// value=option value, onChange=바꾸는 동작, optionList=option
+import { useState } from 'react';
+const sortOptionList=[
+  {value:"latest",name:"최근"},
+  {value:"oldest", name:"등록일"}
+];
+//optionList는 위의 sortOptionList 객체 개별
+const ControlMenu=({value, onChange, optionList})=>{
+  return (
+  <select 
+  value={value} 
+  onChange={(e)=>onChange(e.target.value)}>
+    {optionList.map((it,i)=>(
+      <option key={i} value={it.value}>
+        {it.name}
+        </option>))}
+  </select>
+  );
+}
+
 const DiaryList = ({ diaryList }) => {
+  const [sortType, setSortType]=useState("latest");
+  const getProcessedDirayList =()=>{
+    
+    //비교 함수
+    const compare=(a,b)=>{
+      if(sortType==='latest'){
+        return parseInt(b.date)-parseInt(a.date);
+      }else{
+        return parseInt(a.date)-parseInt(b.date);
+      }
+    };
+
+    const copyList=JSON.parse(JSON.stringify(diaryList));
+    const sortedList=copyList.sort(compare);
+    return sortedList;
+    
+  }
+
+
   return (
     <div>
-      {diaryList.map((it) => (
+    <ControlMenu 
+    value={sortType} 
+    onChange={setSortType} 
+    optionList={sortOptionList}/>
+
+      {getProcessedDirayList().map((it) => (
         <div key={it.id}>{it.content}</div>
       ))}
     </div>
