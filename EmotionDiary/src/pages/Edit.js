c09d4168c434722bbff8af3f5c647325ef9234e9
Xeostop1 <stop1@daum.net>
2022-12-08 18:00:51 +0900

    modify: emotionDiary

--- EmotionDiary/src/pages/Edit.js
+++ EmotionDiary/src/pages/Edit.js
@@ -1,10 +1,51 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
 const Edit = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  //배열을 반환함 쿼리가 가지고 있으니까
+  //set은 쿼리를 바꿀 수 있는 기능을 가지고 있음
+
+  //함수말고 데이터안에서 꺼내야지~
+  const id = searchParams.get("id");
+  console.log("id: ", id);
+
+  const mode = searchParams.get("mode");
+  console.log("mode: ", mode);
+
+  const navigate = useNavigate();
+  //useNavigate()을 함수표현식으로 사용함
+  //주소를 인자로 넣어줌 함수값을 넣어줌
+
+  //비구조화 할당: 배열이나 객체의 속성, 값을 해체! 그 값을 각각 담아 사용!!
+
   return (
     <div>
       <h1>Edit</h1>
       <p>Here is Edit </p>
+      <button onClick={() => setSearchParams({ who: "xeo" })}>쿼리 변경</button>
+      <br />
+      <button
+        onClick={() => {
+          navigate("/home");
+        }}
+      >
+        Home
+      </button>
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        Back
+      </button>
     </div>
   );
 };
 
 export default Edit;
+//함수에 객첼르 전달해야 쿼리에서 꺼내서 보여줄 수 있음
+//오 싱기해!
+//비로그인 사용자가 로그인페이지로 이동 시 강제로 회원가입이나 로그인페이지로 이동
+//***링크를 클릭하지 않아도 개발자가 링크를 변경할 수 있다
+
+//뒤로가기는 -1을 넣어주면 된데 전뎁스로 찾아 가면 되나봐
--- EmotionDiary/src/pages/Home.js
+++ EmotionDiary/src/pages/Home.js
@@ -1,8 +1,72 @@
+import { useContext, useEffect, useState } from "react";
+import { DiaryStateContext } from "../App";
+
+import Myheader from "./../components/MyHeader";
+import MyButton from "./../components/MyButton";
+import DiaryList from "./../components/DiaryList";
+
 const Home = () => {
+  const diaryList = useContext(DiaryStateContext);
+  const [data, setData] = useState([]);
+  const [curDate, setCurDate] = useState(new Date());
+  //배열로 요일 구하기
+  const getDayKorea = (cur) => {
+    let week = ["일", "월", "화", "수", "목", "금", "토"];
+    return week[cur];
+  };
+  const headText = `${curDate.getFullYear()}년 ${
+    curDate.getMonth() + 1
+  }월 ${curDate.getDate()}일 (${getDayKorea(curDate.getDay())})`;
+
+  useEffect(() => {
+    if (diaryList.length >= 1) {
+      const firstDay = new Date(
+        curDate.getFullYear(),
+        curDate.getMonth(),
+        //1일 세팅한 이유는 모든 월의 1일부터 시작하기 위해
+        1
+      ).getTime();
+
+      const lastDay = new Date(
+        curDate.getFullYear(),
+        curDate.getMonth() + 1,
+        0
+      );
+      //비교연산자 사용 &&
+      setData(
+        diaryList.filter((it) => firstDay <= it.date && it.date <= lastDay)
+      );
+    }
+  }, [diaryList, curDate]);
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
+  const increaseMonth = () => {
+    setCurDate(
+      new Date(
+        curDate.getFullYear(),
+        //1달씩 다음달로 이동
+        curDate.getMonth() + 1,
+        curDate.getDate()
+        // 요일은 새로 넣어 주지 않아도 되네! 왜지??
+      )
+    );
+  };
+  const decreaseMonth = () => {
+    setCurDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() - 1, curDate.getDate())
+    );
+  };
   return (
     <div>
-      <h1>Home</h1>
-      <p>Here is Home </p>
+      <Myheader
+        headText={headText}
+        leftChild={<MyButton text={"<"} onClick={decreaseMonth} />}
+        rightChild={<MyButton text={">"} onClick={increaseMonth} />}
+      />
+      <DiaryList diaryList={data} />
     </div>
   );
 };
